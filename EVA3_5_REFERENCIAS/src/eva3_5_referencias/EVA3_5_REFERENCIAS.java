62c2ecbbf142605eed1b85859c81f42fd29221d2
AdanMendoza <143568907+AdanMendoza@users.noreply.github.com>
2023-11-24 17:45:09 -0700

    Add files via upload

---
+++ EVA3_5_REFERENCIAS/src/eva3_5_referencias/EVA3_5_REFERENCIAS.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_5_referencias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_5_REFERENCIAS {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      int[] arreglo = new int[10];
+      System.out.println(arreglo);
+      for (int i : arreglo) {
+         System.out.println(i);
+      }
+      String[] arreCade = new String[10];
+      System.out.println(arreCade);
+      Scanner input = new Scanner(System.in);
+      System.out.println(input);
+   }
+   
+}
